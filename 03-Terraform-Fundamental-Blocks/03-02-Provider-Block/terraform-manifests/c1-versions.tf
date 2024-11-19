b0a4031b0979d21bc4a860845e89beaacdaa41a3
vivekaps16 <vivekaps16@gmail.com>
2024-11-19 18:17:23 +0530

    commit

--- 03-Terraform-Fundamental-Blocks/03-02-Provider-Block/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Fundamental-Blocks/03-02-Provider-Block/terraform-manifests/c1-versions.tf
@@ -1,21 +1,17 @@
-# Terraform Block
 terraform {
-  required_version = ">= 1.4.1"
+  required_version = "~> 0.14.6"
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 4.0"
+      version = "3.0"  
+
     }
   }
 }
 
-# Provider Block
-provider "aws" {
-  region = "us-east-1"  
-  profile = "default" 
-}
 
-/*
-Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
-$HOME/.aws/credentials
-*/
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "default"
+} 
